aeaa900a6a747421e29dacfac5eced64db0b021b
AlejandroZuritaPerez <119274449+AlejandroZuritaPerez@users.noreply.github.com>
2023-04-15 11:30:10 -0600

    Add files via upload

---
+++ Ejercicios3_2POOU3/src/main/java/pag238/PruebaDibujarCaraSonriente.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pag238;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author ALEJANDRO ZURITA PÉREZ
+ */
+public class PruebaDibujarCaraSonriente {
+    
+    public static void main(String[] args) {
+        
+        DibujarCaraSonriente panel = new DibujarCaraSonriente();
+        JFrame aplicacion = new JFrame();
+        
+        aplicacion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aplicacion.add(panel);
+        aplicacion.setSize(230, 250);
+        aplicacion.setVisible(true);
+        aplicacion.setLocationRelativeTo(null);
+    }
+}
